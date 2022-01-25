a61cadb9d5c18e8bc67bbd890511ce1192f42631
MYkola Batsvin <mykola.batsvin@gmail.com>
2022-01-25 19:08:47 +0200

    finished  Diving into Redux

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,36 @@
 import classes from './Auth.module.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { authAction } from '../store';
 
 const Auth = () => {
+  const dispatch = useDispatch()
+
+  const logintHandle = () => {
+    dispatch(authAction.login())
+  }
+
+  const isAuth = useSelector(state => state.auth.isAuth)
   return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
+    <>
+      {!isAuth &&
+        <main className={classes.auth}>
+          <section>
+            <form>
+              <div className={classes.control}>
+                <label htmlFor='email'>Email</label>
+                <input type='email' id='email' />
+              </div>
+              <div className={classes.control}>
+                <label htmlFor='password'>Password</label>
+                <input type='password' id='password' />
+              </div>
+              <button onClick={logintHandle}>Login</button>
+            </form>
+          </section>
+        </main>
+      }
+    </>
   );
 };
 
